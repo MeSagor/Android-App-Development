fc02a54f21536e7d8b1cd8a6f9b3f0e89d379b6a
MeSagor <dear.shorifulislam@gmail.com>
2022-06-22 00:17:50 +0600

    Readme update

--- app/src/main/java/com/example/appnavigation/GameFragment.kt
+++ app/src/main/java/com/example/appnavigation/GameFragment.kt
@@ -22,12 +22,12 @@ class GameFragment : Fragment() {
     // All questions must have four answers.  We'd want these to contain references to string
     // resources so we could internationalize. (or better yet, not define the questions in code...)
     private val questions: MutableList<Question> = mutableListOf(
-            Question(text = "What is Android Jetpack?",
-                    answers = listOf("all of these", "tools", "documentation", "libraries")),
-            Question(text = "Base class for Layout?",
-                    answers = listOf("ViewGroup", "ViewSet", "ViewCollection", "ViewRoot")),
-            Question(text = "Layout for complex Screens?",
-                    answers = listOf("ConstraintLayout", "GridLayout", "LinearLayout", "FrameLayout")),
+            Question(text = "What is your current semester?",
+                    answers = listOf("3.1", "2.2", "3.2", "2.1")),
+            Question(text = "Where is your favorite Dish?",
+                    answers = listOf("Khala-Vhuna", "Grill", "Biriyani", "Khacchi")),
+            Question(text = "Do you have Girl-friend?",
+                    answers = listOf("no", "yes", "noo", "nooo")),
             Question(text = "Pushing structured data into a Layout?",
                     answers = listOf("Data Binding", "Data Pushing", "Set Text", "OnClick")),
             Question(text = "Inflate layout in fragments?",
@@ -38,8 +38,8 @@ class GameFragment : Fragment() {
                     answers = listOf("VectorDrawable", "AndroidVectorDrawable", "DrawableVector", "AndroidVector")),
             Question(text = "Android Navigation Component?",
                     answers = listOf("NavController", "NavCentral", "NavMaster", "NavSwitcher")),
-            Question(text = "Registers app with launcher?",
-                    answers = listOf("intent-filter", "app-registry", "launcher-registry", "app-launcher")),
+            Question(text = "সবসময়ই কার বিচি লক থাকে?",
+                    answers = listOf("সামি", "রাফাত", "জাহিদ", "সাগর")),
             Question(text = "Mark a layout for Data Binding?",
                     answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>"))
     )
