67b4634bc817d29fc7b4035f179e5380f6ee4e35
MeSagor <dear.shorifulislam@gmail.com>
2022-06-21 23:35:16 +0600

    Added Support for the Up Button

--- app/src/main/java/com/example/appnavigation/MainActivity.kt
+++ app/src/main/java/com/example/appnavigation/MainActivity.kt
@@ -20,6 +20,8 @@ import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 //import androidx.navigation.NavController
 //import androidx.navigation.NavDestination
 //import androidx.navigation.Navigation
@@ -33,5 +35,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
