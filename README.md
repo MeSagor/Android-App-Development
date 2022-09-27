793131b8dee9d6a3e32244b6072d3403a04b9d03
MeSagor <dear.shorifulislam@gmail.com>
2022-09-27 22:55:58 +0600

    Weather-app using flutter & api

---
+++ README.md
@@ -0,0 +1,20 @@
+
+<h1 align="center">Android App Development</h1>
+
+This Project is all about learning Android App Development so that I and my class-mates can be `কোটি-পতি পোলা` 😄.
+<br><br>In this Branch`(Assignment_6)`, I made a `Weather-App using Flutter`.
+
+## Course Details
+- Course Name: Software Development Lab III
+- Course Code: CSE-3162
+- Teacher: [MD. OMAR FARUQE](https://github.com/omarfaruqe)
+
+## Built With
+- [Android Studio](https://developer.android.com/studio)
+- [Kotlin](https://developer.android.com/kotlin)
+- [Flutter](https://flutter.dev/)
+
+## Preview
+<p>
+  <img src="preview.jpg" width="200" height="400" title="preview">
+</p>
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+void main() {
+  runApp(
+       const Center(
+        child: Directionality(
+            textDirection: TextDirection.rtl,
+            child: MyWidget()),
+      )
+  );
+}
+
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(context) {
+    return FutureBuilder<String>(
+        future: getData(),
+        builder: (context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            return Text(
+              "Temperature: ${snapshot.data}°C",
+              style: const TextStyle(
+                fontFamily: "Roboto",
+                fontSize: 30.0,
+              ),
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
+        }
+    );
+  }
+}
+
+Future<String> getData() async{
+  String url = 'https://api.openweathermap.org/data/2.5/onecall?lat=24.557663&lon=89.502233&units=metric&exclude=minutely,daily,alerts,hourly&appid=d12df384e504558e0a7db7841f96679b';
+  var response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    var json = jsonDecode(response.body);
+    return json['current']['temp'].toString();
+  } else {
+    throw Exception('Failed to load Data');
+  }
+}
